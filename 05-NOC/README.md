c3f11008cb6716715801a8b3e4432e6513e5dce5
Felipe Vasquez Gomez <felipe.dev1998@gmail.com>
2026-06-02 07:54:26 -0500

    seccion 9 terminada

---
+++ 05-NOC/README.md
@@ -0,0 +1,13 @@
+# PROYECTO NOC
+
+El objetivo es crear una serie de tareas usando Arquitectura Limpia con TypeScript
+
+# dev
+1. Clonar el archivo .env.template a .env
+2. Configurar las variables de entorno
+```
+PORT=3000
+MAILER_EMAIL=soporte@devtalles.com
+MAILER_SECRET_KEY=123456
+PROD=false
+```
--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,5 +1,12 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
+
 
 export class Server {
 
@@ -9,12 +16,13 @@ export class Server {
     CronService.createJob(
       '*/5 * * * * *',
       () => {
-        const url = 'https://www.google.com';
+        const url = 'https://google.com';
         new CheckService(
+          fileSystemLogRepository,
           () => console.log(`${url} is ok`),
           ( error ) => console.log( error )
         ).execute(url);
-        // new CheckService().execute('https://www.google.com');
+        // new CheckService().execute('http://localhost:3000');
       }
     );
 
