3d5256af98b4dfaf4223e66fcc3adae0377ee172
Felipe Vasquez Gomez <felipe.dev1998@gmail.com>
2026-07-30 15:12:30 -0500

    seccion 16 terminada

--- 07-RESTWeb/src/domain/index.ts
+++ 07-RESTWeb/src/domain/index.ts
@@ -1,3 +1,12 @@
 
-export * from './dtos/todos/create-todo.dto';
-export * from './dtos/todos/update-todo.dto';
+export * from './datasources/todo.datasouce';
+export * from './dtos';
+export * from './entities/todo.entity';
+export * from './repositories/todo.repository';
+
+
+export * from './use-cases/todo/create-todo';
+export * from './use-cases/todo/update-todo';
+export * from './use-cases/todo/delete-todo';
+export * from './use-cases/todo/get-todo';
+export * from './use-cases/todo/get-todos';
